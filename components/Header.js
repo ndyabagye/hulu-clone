2e491246137519a61a971ff487b3fab9f6b631a7
ndyabagye <ndyabagyehenrytusi@gmail.com>
2021-08-07 17:10:22 +0300

    started on header and installed heroicons

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,28 @@
+import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+} from '@heroicons/react/outline'
+
+
+
 function header() {
     return (
         <header className="">
-            <h1>This is the header</h1>
+            <div>
+                <HeaderItem title="HOME" Icon={HomeIcon}/>
+            </div>
+            <Image
+                className="object-contain"
+                src="https://links.papareact.com/ua6"
+                width={200}
+                height={100}
+            />
         </header>
     )
 }
